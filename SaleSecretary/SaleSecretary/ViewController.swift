e89d54844253cdbf2d4c7d02570aef013f611084
liusheding <liusheding@lsd-mac.local>
2017-07-26 10:26:44 +0800

    main scenes

---
+++ SaleSecretary/SaleSecretary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaleSecretary
+//
+//  Created by liusheding on 2017/7/26.
+//  Copyright © 2017年 zjjy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
