742cf19bda7c0a752d6a5c99282e5e7268be49ab
liusheding <liusheding@lsd-mac.local>
2017-07-27 09:35:14 +0800

    add frameworks

---
+++ SaleSecretary/SaleSecretary/ToolsUIViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ToolsUIViewController.swift
+//  SaleSecretary
+//
+//  Created by liusheding on 2017/7/26.
+//  Copyright © 2017年 zjjy. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import SwiftyJSON
+
+class ToolsUIViewController : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+
+extension ToolsUIViewController {
+    
+    func setupUI() {
+        
+    }
+}
