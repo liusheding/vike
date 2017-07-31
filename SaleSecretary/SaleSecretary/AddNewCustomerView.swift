8364f99a04bead146109eb7ddac787d6e4cba4c5
Lutiguang <lutiguang@LutiguangdeMacBook-Pro.local>
2017-07-31 14:05:25 +0800

    add  新客户

---
+++ SaleSecretary/SaleSecretary/AddNewCustomerView.swift
@@ -0,0 +1,21 @@
+//
+//  AddNewCustomerView.swift
+//  SaleSecretary
+//
+//  Created by Lutiguang on 2017/7/31.
+//  Copyright © 2017年 zjjy. All rights reserved.
+//
+
+import UIKit
+
+class AddNewCustomerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
