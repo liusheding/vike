053eab46a216011fde60a3f822e21ac2786728e7
xiaoqiang03 <30405957+xiaoqiang03@users.noreply.github.com>
2017-08-31 16:57:04 +0800

    change pwd

--- SaleSecretary/SaleSecretary/ChangePwdController.swift
+++ SaleSecretary/SaleSecretary/ChangePwdController.swift
@@ -19,6 +19,10 @@ class ChangePwdController: UIViewController {
         newpwd.borderStyle = .none
         newpwd2.borderStyle = .none
         
+        oldpwd.isSecureTextEntry = true
+        newpwd.isSecureTextEntry = true
+        newpwd2.isSecureTextEntry = true
+        
         oldpwd.placeholder = "请输入旧密码"
         newpwd.placeholder = "请输入新密码"
         newpwd2.placeholder = "请再一次输入新密码"
@@ -44,7 +48,37 @@ class ChangePwdController: UIViewController {
     }
     
     func clickSaveBtn(){
-        print("====save======")
+        let oldpwdtext = oldpwd.text
+        if oldpwdtext == ""{
+            showAlert("请输入原始密码")
+            return
+        }
+        let newpwdtext = newpwd.text
+        let newpwd2text = newpwd2.text
+        if newpwdtext == "" || newpwd2text == ""{
+            showAlert("请输入新密码")
+            return
+        }
+        if newpwd.text != newpwd2.text{
+            showAlert("两次输入密码不一致")
+            return
+        }
+        
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
+        let body = ["busi_scene":"MODIFY_PWD", "id": APP_USER_ID, "originalPwd":oldpwdtext, "loginPwd":newpwdtext]
+        let request = NetworkUtils.postBackEnd("U_USER", body: body , handler: {[weak self] (val ) in
+            self?.showAlert("密码修改成功，请重新登录")
+            AppUser.logout()
+        })
+        request.response(completionHandler: {_ in
+            self.navigationItem.rightBarButtonItem?.isEnabled = true
+        })
+    }
+    
+    func showAlert(_ message:String){
+        let alertController = UIAlertController(title: "提示", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "好的", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
-
