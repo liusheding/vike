06cddf235247b479e2058770190842d4e58d9209
xiaoqiang03 <30405957+xiaoqiang03@users.noreply.github.com>
2017-09-05 16:42:19 +0800

    tixian

--- SaleSecretary/SaleSecretary/TakeoutMoneyViewController.swift
+++ SaleSecretary/SaleSecretary/TakeoutMoneyViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class TakeoutMoneyViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lastmoney: UILabel!
     @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var banknum: UITextField!
     @IBAction func clickTakeAll(_ sender: UIButton) {
         textfield.text = lastmoney.text
     }
     @IBAction func clickSubmit(_ sender: UIButton) {
+        let bankstr = banknum.text!
+        if bankstr == ""{
+            showAlert("请填写本人银行卡号")
+            return
+        }
+        let bankarray = bankstr.components(separatedBy: ".")
+        if bankarray.count != 0{
+            showAlert("请填写正确的银行卡号")
+            return
+        }
+        
         let str = textfield.text!
         if str == ""{
             showAlert("请填写提现金额")
@@ -51,7 +64,16 @@ class TakeoutMoneyViewController: UIViewController {
         
         textfield.borderStyle = .none
         textfield.keyboardType = .decimalPad
-        textfield.becomeFirstResponder()
+        textfield.placeholder = "请输入提现金额"
+        
+        banknum.borderStyle = .none
+        banknum.keyboardType = .decimalPad
+        banknum.becomeFirstResponder()
+        banknum.placeholder = "请输入本人银行卡号"
+        
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // 创建一个导航栏
         let navBar = UINavigationBar(frame: CGRect(x:0, y:0, width:self.view.frame.size.width, height:60))
@@ -89,4 +111,28 @@ class TakeoutMoneyViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension TakeoutMoneyViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "payCell")
+        cell.textLabel?.text = "仅限使用本人银行卡提现"
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.textLabel?.textColor = UIColor.darkGray
+        cell.detailTextLabel?.text = "查看可支持银行"
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
 }
