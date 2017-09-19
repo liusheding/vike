a7fabdf2badb1420c7dfac99cc8c83c30a24bcf6
xiaoqiang03 <30405957+xiaoqiang03@users.noreply.github.com>
2017-09-19 10:54:19 +0800

    new user help

---
+++ SaleSecretary/SaleSecretary/NewUserHelpController.swift
@@ -0,0 +1,33 @@
+//
+//  NewUserHelpController.swift
+//  SaleSecretary
+//
+//  Created by xiaoqiang on 2017/9/19.
+//  Copyright © 2017年 zjjy. All rights reserved.
+//
+
+import UIKit
+
+class NewUserHelpController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1.设置访问资源 - 百度搜索
+        let url = URL(string: "http://www.zj.vc");
+        
+        // 2.建立网络请求
+        let request = URLRequest(url: url!);
+        
+        // 3.加载网络请求
+        webView.loadRequest(request)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
