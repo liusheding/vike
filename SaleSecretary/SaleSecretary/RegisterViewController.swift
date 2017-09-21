4604638a1b23738661cb01d40a0ee28b8d41a492
xiaoqiang03 <30405957+xiaoqiang03@users.noreply.github.com>
2017-09-21 13:16:47 +0800

    NavigationBar

--- SaleSecretary/SaleSecretary/RegisterViewController.swift
+++ SaleSecretary/SaleSecretary/RegisterViewController.swift
@@ -18,6 +18,9 @@ class RegisterViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var recommendCode: UITextField!
     @IBOutlet weak var registerBtn: UIButton!
     @IBOutlet weak var smsBtn: UIButton!
+    @IBAction func cancelBtn(_ sender: UIBarButtonItem) {
+        self.clickCancelBtn()
+    }
     
     var timer: Timer?
     
@@ -69,32 +72,6 @@ class RegisterViewController: UIViewController,UITextFieldDelegate {
         
         phone.delegate = self
         smsCheck.delegate = self
-        
-        // 创建一个导航栏
-        let navBar = UINavigationBar(frame: CGRect(x:0, y:0, width:self.view.frame.size.width, height:60))
-        // 导航栏背景颜色
-        navBar.backgroundColor = UIColor.groupTableViewBackground
-        // 自定义导航栏的title，用UILabel实现
-        let titleLabel = UILabel(frame: CGRect(x:0,y:0,width:50,height:60))
-        titleLabel.text = "注册"
-        
-        // 创建导航栏组件
-        let navItem = UINavigationItem()
-        // 设置自定义的title
-        navItem.titleView = titleLabel
-       
-        // 创建左侧按钮
-        let leftButton = UIBarButtonItem(title: "取消", style: .plain, target: self, action: #selector(clickCancelBtn))
-        leftButton.setTitleTextAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 17),NSForegroundColorAttributeName: APP_THEME_COLOR],for: .normal)
-        
-        // 添加左侧按钮
-        navItem.setLeftBarButton(leftButton, animated: false)
-        navigationItem.setHidesBackButton(true, animated: false)
-        // 把导航栏组件加入导航栏
-        navBar.pushItem(navItem, animated: false)
-        
-        // 导航栏添加到view上
-        self.view.addSubview(navBar)
         
         //点击空白收起键盘
         self.view.addGestureRecognizer(UITapGestureRecognizer(target:self, action:#selector(self.handleTap(sender:))))
--- SaleSecretary/SaleSecretary/UserProtocolController.swift
+++ SaleSecretary/SaleSecretary/UserProtocolController.swift
@@ -10,35 +10,12 @@ import UIKit
 
 class UserProtocolController: UIViewController {
     @IBOutlet weak var webView: UIWebView!
+    @IBAction func backBtn(_ sender: UIBarButtonItem) {
+        self.clickCancelBtn()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 创建一个导航栏
-        let navBar = UINavigationBar(frame: CGRect(x:0, y:0, width:self.view.frame.size.width, height:60))
-        // 导航栏背景颜色
-        navBar.backgroundColor = UIColor.groupTableViewBackground
-        // 自定义导航栏的title，用UILabel实现
-        let titleLabel = UILabel(frame: CGRect(x:0,y:0,width:50,height:60))
-        titleLabel.text = "用户服务协议"
-        
-        // 创建导航栏组件
-        let navItem = UINavigationItem()
-        // 设置自定义的title
-        navItem.titleView = titleLabel
-        
-        // 创建左侧按钮
-        let leftButton = UIBarButtonItem(title: "返回", style: .plain, target: self, action: #selector(clickCancelBtn))
-        leftButton.setTitleTextAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 17),NSForegroundColorAttributeName: APP_THEME_COLOR],for: .normal)
-        
-        // 添加左侧按钮
-        navItem.setLeftBarButton(leftButton, animated: false)
-        navigationItem.setHidesBackButton(true, animated: false)
-        // 把导航栏组件加入导航栏
-        navBar.pushItem(navItem, animated: false)
-        
-        // 导航栏添加到view上
-        self.view.addSubview(navBar)
         
         // 1.设置访问资源 - 百度搜索
         let url = URL(string: "http://www.zj.vc");
